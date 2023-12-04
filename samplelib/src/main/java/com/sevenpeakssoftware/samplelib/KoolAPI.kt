c2da60e6efb02b502f5360f9703c302ab3d90dd1
Zaw Myo Naing (Rahul Kumar) <zawmyo.naing@7peakssoftware.com>
2023-12-04 11:15:30 +0700

    Initial commit

---
+++ samplelib/src/main/java/com/sevenpeakssoftware/samplelib/KoolAPI.kt
@@ -0,0 +1,11 @@
+package com.sevenpeakssoftware.samplelib
+
+import android.util.Log
+
+object KoolAPI {
+
+    private const val TAG = "KoolAPI"
+    fun doSomethingAwesome(){
+        Log.i(TAG, "Doing something awesome. 😎")
+    }
+}
